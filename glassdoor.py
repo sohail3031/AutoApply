0df02e7eb22e9d2ad924daa53408b407ef612840
sohail3031 <mohdsohailahmed46@gmail.com>
2025-07-27 03:40:48 -0400

    Check User LoggedIn

--- glassdoor.py
+++ glassdoor.py
@@ -1,10 +1,14 @@
 from colorama import init, Fore
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver import Firefox
 
 class GlassDoor:
     def __init__(self) -> None:
         init(autoreset=True)
 
         self.__check_user_login: bool = bool()
+        self.__web_driver: Firefox = None
 
     @staticmethod
     def __display_options() -> None:
@@ -13,8 +17,23 @@ class GlassDoor:
               "\n1. Apply Using URL"
               "\n2. Apply with Job Search")
 
+    def __set_up(self) -> None:
+        __options: Options = Options()
+        __profile_path: str = input(Fore.BLUE + "\nEnter the Profile Path of Firefox: ")
+
+        __options.set_preference("profile", __profile_path)
+        __options.add_argument("-profile")
+        __options.add_argument(__profile_path)
+
+        __service: Service =Service(executable_path=f"geckodriver-v0.36.0-win64/geckodriver.exe")
+        self.__web_driver = Firefox(service=__service, options=__options)
+
     def __check_if_user_is_logged_in(self) -> None:
-        pass
+        self.__set_up()
+
+        self.__web_driver.get("https://glassdoor.ca")
+
+        print(self.__web_driver.title)
 
     def __apply_using_url(self) -> None:
         if not self.__check_user_login:
